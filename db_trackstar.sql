7ecf9b1ace2716a3cad719e8253fb2f01cb3aeb2
manjul <07.manutdilam@gmail.com>
2013-12-16 20:12:02 +0545

    Updated database for iteration 3..pending

--- db_trackstar.sql
+++ db_trackstar.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 04, 2013 at 09:33 PM
+-- Generation Time: Dec 16, 2013 at 08:04 PM
 -- Server version: 5.5.34-0ubuntu0.13.10.1
 -- PHP Version: 5.5.3-1ubuntu2
 
@@ -19,6 +19,31 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_trackstar`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_issue`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_issue` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) NOT NULL,
+  `description` varchar(2000) DEFAULT NULL,
+  `project_id` int(11) DEFAULT NULL,
+  `type_id` int(11) DEFAULT NULL,
+  `status_id` int(11) DEFAULT NULL,
+  `owner_id` int(11) DEFAULT NULL,
+  `requester_id` int(11) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `create_user_id` int(11) DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `update_user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_issue_project` (`project_id`),
+  KEY `FK_issue_owner` (`owner_id`),
+  KEY `FK_issue_requester` (`requester_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -43,6 +68,72 @@ CREATE TABLE IF NOT EXISTS `tbl_project` (
 
 INSERT INTO `tbl_project` (`id`, `name`, `description`, `create_time`, `create_user_id`, `update_time`, `update_user_id`) VALUES
 (1, 'Test Project', 'Test Project Description', '0000-00-00 00:00:00', NULL, '0000-00-00 00:00:00', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_project_user_assignment`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_project_user_assignment` (
+  `project_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `create_user_id` int(11) DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `update_user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`project_id`,`user_id`),
+  KEY `FK_user_
+project` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(256) NOT NULL,
+  `username` varchar(256) DEFAULT NULL,
+  `password` varchar(256) DEFAULT NULL,
+  `last_login_time` datetime DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `create_user_id` int(11) DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `update_user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id`, `email`, `username`, `password`, `last_login_time`, `create_time`, `create_user_id`, `update_time`, `update_user_id`) VALUES
+(1, 'test1@test.com', 'test1', '5a105e8b9d40e1329780d62ea2265d8a', NULL, NULL, NULL, NULL, NULL),
+(2, 'test2@test.com', 'test2', 'ad0234829205b9033196ba818f7a872b', NULL, NULL, NULL, NULL, NULL);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbl_issue`
+--
+ALTER TABLE `tbl_issue`
+  ADD CONSTRAINT `FK_issue_owner` FOREIGN KEY (`owner_id`) REFERENCES `tbl_user` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_issue_project` FOREIGN KEY (`project_id`) REFERENCES `tbl_project` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_issue_requester` FOREIGN KEY (`requester_id`) REFERENCES `tbl_user` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `tbl_project_user_assignment`
+--
+ALTER TABLE `tbl_project_user_assignment`
+  ADD CONSTRAINT `FK_project_
+user` ADD FOREIGN KEY (`project_id`) REFERENCES `tbl_project` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_user_
+project` ADD FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`id`) ON DELETE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
